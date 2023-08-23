122a3c0162fc06d4d6bfe0c461e7f041bbb49663
AndreVKF <andrekv87@gmail.com>
2023-08-23 17:46:42 -0300

    2023-08-23 - End of day

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import theme from "./styles/theme"
+import { ThemeProvider } from "styled-components"
+import GlobalStyle from "./styles/global"
+
+import Home from "./pages/Home"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Home />
+    </ThemeProvider>
+  </React.StrictMode>
 )
